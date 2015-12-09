21468be48e95ea7c127e791422be77626998a8af
User <sdffs>
2015-12-09 05:18:35 +0200

    Keep Advencing

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,16 +1,76 @@
 function create_event(){
-	title = $("#event_title").val(); 
-	desc = $("#event_desc").val(); 
-	$.ajax({ 
-    	type: 'GET',
-    	url: "/create_event", 
-    	data: { event_title: title, event_desc: desc }, 
-    	dataType: 'json',
-    	success: function(result){
-            console.log(result)
-        	if(result){
-        		window.location.replace("eventpage.html")
-        	}
-    	}
+    title = $("#event_title").val(); 
+    desc = $("#event_desc").val(); 
+    $.ajax({ 
+        type: 'GET',
+        url: "/create_event", 
+        data: { event_title: title, event_desc: desc }, 
+        dataType: 'json',
+        success: function(result){
+            if(result && result["event_title"] && result["event_desc"]){
+                $("#change_header").html("<h1>Title : "+ result["event_title"]+"</h1><h1>Description:"+ result["event_desc"]+"</h1></br><h1>Participants:</h1><h3>Me</h3><h3>Him</h3>");
+            }
+        }
+    });
+}
+function get_events(){
+    $.ajax({ 
+        type: 'GET',
+        url: "/get_events", 
+        data: {}, 
+        dataType: 'json',
+        success: function(result){
+            if(result && result.length){
+                $("#change_header").html(""); 
+                for (var i = 0; i < result.length; i++){
+                    event = result[i];
+                    $("#change_header").append("<a href='#' onclick='join_chat_check(" + i + ")'><h1>"+ event[0] + "</h1></a>");
+                    $("#change_header").append("<p>" + event[1] + "</p><hr>");
+                }
+            }
+        }
+    });
+}
+
+function join_chat_check(num_event){
+    console.log(num_event);
+    $("#change_header").html("Your Name: <input type='text' id='name'>"); 
+    $("#change_header").append("<p>Do you want to join the event?<p>"); 
+    $("#change_header").append("<button onclick='back_to_feed()' value='Yes'>");
+    $("#change_header").append("<button onclick='join_chat( " + num_event + ")' value='No'>");
+}
+function back_to_feed(){
+    window.location.replace("feed.html");
+}
+function join_chat(Num_event){
+    var Name = $("#name").val();
+    $.ajax({ 
+        type: 'GET',
+        url: "/join_chat", 
+        data: {user_name: Name, num_event: Num_event}, 
+        dataType: 'json',
+        success: function(result){
+            if (result){
+                get_chat(Num_event);
+            }
+        }
+    });
+}
+
+function get_chat(Num_event){
+    $.ajax({ 
+        type: 'GET',
+        url: "/get_chat", 
+        data: {num_event: Num_event}, 
+        dataType: 'json',
+        success: function(result){
+            if(result && result.length){
+                $("#change_header").html(""); 
+                $("#change_header").append("<h1>Members:</h1>");            
+                for (var i = 0; i < result.length; i++){
+                    $("#change_header").append("<p>"+ result[i] + "</p>");
+                }
+            }
+        }
     });
 }
--- server.js
+++ server.js
@@ -1,33 +1,48 @@
 var express = require('express');
 var app = express();
 
+var events = [];
+
 app.use(express.static('public'));
 
 app.get('', function (req, res) {
-  console.log('Welcome')
   app.use(express.static( __dirname + "/public"));
-})
+});
 
 app.get('/create_event', function (req, res) {
 
    // Prepare output in JSON format
+   events.push([req.query.event_title, req.query.event_desc, []]);
    response = {
        event_title:req.query.event_title,
        event_desc:req.query.event_desc
    };
-   console.log(response);
    res.send(JSON.stringify(response));
-})
+});
 
-app.get('/Feed.html', function (req, res) {
+app.get('/join_chat', function (req, res) {
 
-})
+   // Prepare output in JSON format
+   events[req.query.num_event][2].push(req.query.user_name);
+   res.send(JSON.stringify("OK"));
+});
+
+app.get('/get_events', function (req, res) {
+    res.send(JSON.stringify(events));
+});
+
+app.get('/get_chat', function (req, res) {
+
+   // Prepare output in JSON format
+   console.log(events);
+   res.send(JSON.stringify(events[req.query.num_event][2]));
+});
 
 var server = app.listen(8080, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Example app listening at http://%s:%s", host, port);
 
-})
+});
