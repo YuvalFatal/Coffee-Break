f46e3d53e0deb1181ef78cc9746d13cc141cba2e
User <sdffs>
2015-12-09 06:37:32 +0200

    Almost done the hackathon

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,30 +1,38 @@
 function create_event(){
     title = $("#event_title").val(); 
     desc = $("#event_desc").val(); 
+
     $.ajax({ 
         type: 'GET',
         url: "/create_event", 
         data: { event_title: title, event_desc: desc }, 
         dataType: 'json',
+
         success: function(result){
             if(result && result["event_title"] && result["event_desc"]){
-                $("#change_header").html("<h1>Title : "+ result["event_title"]+"</h1><h1>Description:"+ result["event_desc"]+"</h1></br><h1>Participants:</h1><h3>Me</h3><h3>Him</h3>");
+                $("#change_header").html("<p>Title: <h4>"+ result["event_title"]+"</h4></p>");
+                $("#change_header").append("<p>Description: <h4>"+ result["event_desc"]+"</h4></p>")
+                $("#change_header").append("</br><h3>Your event has been created!</h3>")
             }
         }
     });
 }
+
+
 function get_events(){
     $.ajax({ 
         type: 'GET',
         url: "/get_events", 
         data: {}, 
         dataType: 'json',
+
         success: function(result){
             if(result && result.length){
                 $("#change_header").html(""); 
+
                 for (var i = 0; i < result.length; i++){
                     event = result[i];
-                    $("#change_header").append("<a href='#' onclick='join_chat_check(" + i + ")'><h1>"+ event[0] + "</h1></a>");
+                    $("#change_header").append("<h3><a href='#' onclick='join_chat_check(" + i + ")'>"+ event[0] + "</a></h3>");
                     $("#change_header").append("<p>" + event[1] + "</p><hr>");
                 }
             }
@@ -32,23 +40,29 @@ function get_events(){
     });
 }
 
+
 function join_chat_check(num_event){
-    console.log(num_event);
-    $("#change_header").html("Your Name: <input type='text' id='name'>"); 
+    $("#change_header").html("<p>Your Name:</p> <input type='text' id='name'>"); 
     $("#change_header").append("<p>Do you want to join the event?<p>"); 
-    $("#change_header").append("<button onclick='back_to_feed()' value='Yes'>");
-    $("#change_header").append("<button onclick='join_chat( " + num_event + ")' value='No'>");
+    $("#change_header").append("<input type='submit' onclick='back_to_feed()' value='No'></br>");
+    $("#change_header").append("</br><input type='submit' onclick='join_chat( " + num_event + ")' value='Yes'>");
 }
+
+
 function back_to_feed(){
     window.location.replace("feed.html");
 }
+
+
 function join_chat(Num_event){
     var Name = $("#name").val();
+
     $.ajax({ 
         type: 'GET',
         url: "/join_chat", 
         data: {user_name: Name, num_event: Num_event}, 
         dataType: 'json',
+
         success: function(result){
             if (result){
                 get_chat(Num_event);
@@ -57,16 +71,19 @@ function join_chat(Num_event){
     });
 }
 
+
 function get_chat(Num_event){
     $.ajax({ 
         type: 'GET',
         url: "/get_chat", 
         data: {num_event: Num_event}, 
         dataType: 'json',
+
         success: function(result){
             if(result && result.length){
                 $("#change_header").html(""); 
-                $("#change_header").append("<h1>Members:</h1>");            
+                $("#change_header").append("<h3>Members In Event:</h3>"); 
+
                 for (var i = 0; i < result.length; i++){
                     $("#change_header").append("<p>"+ result[i] + "</p>");
                 }
